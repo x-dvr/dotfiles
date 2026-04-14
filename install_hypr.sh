a79e83163b1ece488d622870aae24fd3eabad255
Denis Rodin <denis.rodin@proton.me>
2026-04-14 20:09:48 +0200

    feat: Improve logout in hyprland

--- install_hypr.sh
+++ install_hypr.sh
@@ -5,5 +5,5 @@ sudo pacman -Syu
 sudo pacman -S hyprland xdg-desktop-portal-hyprland hyprlock hypridle
 sudo pacman -S hyprlauncher hyprpaper hyprpolkitagent waybar hyprshot
 sudo pacman -S swaync # remove dunst if needed
-
+paru -S hyprshutdown
 paru -Sy nwg-look catppuccin-gtk-theme-frappe
