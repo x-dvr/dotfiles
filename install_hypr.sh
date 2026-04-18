938ab71f34f896bc8127b72687712ef6fdf6260e
Denis Rodin <denis.rodin@proton.me>
2026-04-18 16:01:26 +0200

    feat: Small hyprland tweaks

--- install_hypr.sh
+++ install_hypr.sh
@@ -3,9 +3,10 @@
 sudo pacman -Syu
 
 sudo pacman -S hyprland xdg-desktop-portal-hyprland hyprlock hypridle
-sudo pacman -Sy wl-clipboard wmenu grim slurp swaybg ttf-jetbrains-mono-nerd
+sudo pacman -Sy wl-clipboard grim slurp swaybg ttf-jetbrains-mono-nerd
 sudo pacman -S hyprlauncher hyprpaper hyprpolkitagent waybar hyprshot
 sudo pacman -S swaync # remove dunst if needed
 
+paru -S qt5-wayland qt6-wayland
 paru -S hyprshutdown
 paru -Sy nwg-look catppuccin-gtk-theme-frappe
