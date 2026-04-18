b1de738b6689f386376f2c9b1f55056cb7218971
Denis Rodin <denis.rodin@proton.me>
2026-04-18 15:05:20 +0200

    feat: Claude config

--- install_claude.sh
+++ install_claude.sh
@@ -3,10 +3,13 @@
 # install claude-code
 curl -fsSL https://claude.ai/install.sh | bash
 
+rm -rf ~/.claude
+
+cd ~/dotfiles
+stow claude
 # install status line
-cp ./statusline.sh ~/.claude/statusline.sh
 chmod +x ~/.claude/statusline.sh
 
 # 2. Add to ~/.claude/settings.json
-jq '. + {"statusLine": {"type": "command", "command": "~/.claude/statusline.sh", "padding": 0}}' \
-  ~/.claude/settings.json > /tmp/settings.json && mv /tmp/settings.json ~/.claude/settings.json
+# jq '. + {"statusLine": {"type": "command", "command": "~/.claude/statusline.sh", "padding": 0}}' \
+#   ~/.claude/settings.json > /tmp/settings.json && mv /tmp/settings.json ~/.claude/settings.json
